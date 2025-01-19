6ecd792475c4c1b2e39a62a98bd0961ec9482340
Adam Fidel <adam@fidel.id>
2025-01-19 12:06:57 -0600

    fix: icons for volume notifications

--- files/scripts/29-finalize-de-tweaks.sh
+++ files/scripts/29-finalize-de-tweaks.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -ouex pipefail
+set -oeux pipefail
 
 # Battery panel icon tweaks (stay on full icon from 100% to 90%)
 cd /usr/share/icons/Chicago95
@@ -12,6 +12,14 @@ for f in $(find . -name "battery-level-90*"); do
     rm $icon_name
     ln -s ${icon_name/90/100} $icon_name
   fi
+done
+
+# TODO: remove when https://github.com/grassmunk/Chicago95/pull/362 is merged
+# Fix missing icons in xfce4-notifyd for volume
+cd /usr/share/icons/Chicago95/status/32
+for icon in audio-volume-*; do
+  cd ../symbolic
+  ln -s ../32/$icon $icon
 done
 
 update-mime-database /usr/share/mime
