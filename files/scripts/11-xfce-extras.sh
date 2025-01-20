28dfd4f61162aec521248830992805f415b3b4b9
Adam Fidel <adam@fidel.id>
2025-01-20 16:42:41 -0600

    feat: use skippy-xd by default instead of xfdashboard

---
+++ files/scripts/11-xfce-extras.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -xueo pipefail
+
+# TODO: package this in COPR instead of installing dev libraries
+dnf install -y libXft-devel libXrender-devel libXcomposite-devel libXdamage-devel libXfixes-devel libXext-devel libXinerama-devel libpng-devel libjpeg-turbo-devel giflib-devel
+cd /tmp
+git clone https://github.com/felixfung/skippy-xd.git
+cd skippy-xd
+make
+make install
+dnf remove -y libXft-devel libXrender-devel libXcomposite-devel libXdamage-devel libXfixes-devel libXext-devel libXinerama-devel libpng-devel libjpeg-turbo-devel giflib-devel
+
+systemctl --global enable skippy-xd.service
