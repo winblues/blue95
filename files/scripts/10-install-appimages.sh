87d7aabc16d0aee5c9c55130f7e116bc1af61f8a
Adam Fidel <adam@fidel.id>
2025-01-21 17:42:22 -0600

    feat: replace bauh with Easy Flatpak

--- files/scripts/10-install-appimages.sh
+++ files/scripts/10-install-appimages.sh
@@ -2,8 +2,6 @@
 
 set -xueo pipefail
 
-curl -Lo /usr/bin/bauh https://github.com/vinifmor/bauh/releases/download/0.10.7/bauh-0.10.7-x86_64.AppImage
-chmod +x /usr/bin/bauh
 
 cd /tmp
 curl -Lo palemoon.tar.xz "https://www.palemoon.org/download.php?mirror=us&bits=64&type=linuxgtk3"
