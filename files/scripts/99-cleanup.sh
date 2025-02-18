bb6351d254b6c648dfd70a1559fd31d78d96cc89
Adam Fidel <adam@fidel.id>
2025-02-17 19:10:42 -0600

    feat: add some vm packages

--- files/scripts/99-cleanup.sh
+++ files/scripts/99-cleanup.sh
@@ -2,3 +2,5 @@
 set -xueo pipefail
 
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/tailscale.repo
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/negativo17-fedora-multimedia.repo
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_ublue-os_staging.repo
