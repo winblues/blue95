e5b3a81d63d192a1db404240fa9636429c2bacac
Adam Fidel <adam@fidel.id>
2025-02-28 11:44:47 -0600

    add service and preset

--- files/scripts/11-chezmoi.sh
+++ files/scripts/11-chezmoi.sh
@@ -9,4 +9,4 @@ mkdir -p /usr/share/winblues/chezmoi/dot_local/share/xfconf-profile
 cp /usr/share/xfconf-profile/default.json \
   /usr/share/winblues/chezmoi/dot_local/share/winblues-blue95.json
 
-systemctl enable --global winblues-chezmoi.service
+systemctl --global preset-all
