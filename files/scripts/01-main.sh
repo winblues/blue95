22863028a544175b38b65b46dffd2876146eaf46
Adam Fidel <ledif@users.noreply.github.com>
2025-02-28 14:37:43 -0600

    feat: add chezmoi service (#3)

--- files/scripts/01-main.sh
+++ files/scripts/01-main.sh
@@ -3,4 +3,12 @@
 set -ouex pipefail
 
 systemctl enable libvirtd.service
-systemctl --global enable xfconf-profile.service
+
+# This profile will not actually be used by xfconf-profile. We are
+# only moving it to the chezmoi target state because we want to apply
+# chezmoi changes if this file changes
+mkdir -p /usr/share/winblues/chezmoi/dot_local/share/xfconf-profile
+cp /usr/share/xfconf-profile/default.json \
+  /usr/share/winblues/chezmoi/dot_local/share/winblues-blue95.json
+
+systemctl --global preset-all
