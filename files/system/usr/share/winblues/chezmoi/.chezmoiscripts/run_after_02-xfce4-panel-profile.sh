858994cfed4a4377d675c50103c3fbbec5719f9a
Adam Fidel <adam@fidel.id>
2025-03-27 22:23:46 -0500

    fix: re-enable xfce4-panel-profiles in chezmoi

--- files/system/usr/share/winblues/chezmoi/.chezmoiscripts/run_after_02-xfce4-panel-profile.sh
+++ files/system/usr/share/winblues/chezmoi/.chezmoiscripts/run_after_02-xfce4-panel-profile.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-echo "Skipping xfce4-panel-profiles for now"
-exit 0
+#echo "Skipping xfce4-panel-profiles for now"
+#exit 0
 
 xfce4-panel-profiles load /usr/share/xfce4-panel-profiles/layouts/chicago-95.tar.bz2
