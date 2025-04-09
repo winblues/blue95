e8a5a9cd9c32e4a2395749a3d9579868e417804a
Adam Fidel <adam@fidel.id>
2025-04-09 17:52:54 -0500

    apply xfconf-profile

--- files/system/usr/share/winblues/chezmoi/.chezmoiscripts/run_after_00-etc-skel.sh
+++ files/system/usr/share/winblues/chezmoi/.chezmoiscripts/run_after_00-etc-skel.sh
@@ -7,10 +7,14 @@ set -euxo pipefail
 state_dir="${XDG_STATE_HOME:-$HOME/.local/state}/winblues"
 marker_file="$state_dir/chezmoi-blue95"
 
+# If this is the first time we've run this, disable the service
+# and reset the user's configs from /etc/skel
 if [[ ! -f "$marker_file" ]]; then
   systemctl --user mask winblues-chezmoi.service
 
+  # Copy pristine user config and apply xfconf-profile
   rsync -av --progress /etc/skel/ $HOME
+  xfconf-profile apply --merge=hard /usr/share/xfconf-profile/default.json
 
   # Remove old state files from other Winblues variants
   rm -f "$state_dir"/chezmoi-*
