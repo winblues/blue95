d7ced1696c7efaec3e80d866889742c1ab04b4b3
Adam Fidel <adam@fidel.id>
2025-04-16 13:48:51 -0500

    rework how scripts are called

--- files/scripts/90-finalize.sh
+++ files/scripts/90-finalize.sh
@@ -3,7 +3,7 @@ set -ouex pipefail
 
 update-mime-database /usr/share/mime
 gdk-pixbuf-query-loaders-64 --update-cache
-#gtk-update-icon-cache /usr/share/icons/hicolor
+gtk-update-icon-cache /usr/share/icons/hicolor
 gtk-update-icon-cache --force --ignore-theme-index /usr/share/icons/Chicago95
 
 systemctl --global preset-all
