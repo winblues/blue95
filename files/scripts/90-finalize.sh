3c0fdf61731edc97073aa8e600bf0a2ec6b34042
Adam Fidel <adam@fidel.id>
2025-04-16 10:38:48 -0500

    fix: move icon cache update to finalize

--- files/scripts/90-finalize.sh
+++ files/scripts/90-finalize.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -ouex pipefail
 
+update-mime-database /usr/share/mime
+gdk-pixbuf-query-loaders-64 --update-cache
+gtk-update-icon-cache --force --ignore-theme-index /usr/share/icons/Chicago95
+
 systemctl --global preset-all
