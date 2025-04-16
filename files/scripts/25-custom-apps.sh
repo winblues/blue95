02a45b707053f5923986838c86c6f3c871883089
Adam Fidel <adam@fidel.id>
2025-04-16 11:21:12 -0500

    fix: add icon to Chicago95

--- files/scripts/25-custom-apps.sh
+++ files/scripts/25-custom-apps.sh
@@ -7,3 +7,5 @@ curl -Lo paint.zip https://github.com/winblues/paint/archive/refs/heads/main.zip
 unzip paint.zip
 cd paint-main
 make install
+
+cp /usr/share/icons/hicolor/scalable/apps/win.blues.paint.svg /usr/share/icons/Chicago95/apps/scalable/win.blues.paint.svg
