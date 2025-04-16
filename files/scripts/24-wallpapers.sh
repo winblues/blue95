6eef2dd209fb2e037328161c96b6839a77fff3e2
Adam Fidel <ledif@users.noreply.github.com>
2025-04-15 23:49:01 -0500

    feat: add Windows 3.0 and 3.1 wallpapers (#54)

---
+++ files/scripts/24-wallpapers.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+set -oeux pipefail
+
+URLS=(
+  "https://archive.org/download/microsoft-windows-wallpapers-pictures/1-2.%20Windows%203.1x%20and%20NT%203.x/256color%20%28large%29.bmp"
+  "https://archive.org/download/microsoft-windows-wallpapers-pictures/1-2.%20Windows%203.1x%20and%20NT%203.x/Concrete.bmp"
+  "https://archive.org/download/microsoft-windows-wallpapers-pictures/1-2.%20Windows%203.1x%20and%20NT%203.x/FLOCK.BMP"
+  "https://archive.org/download/microsoft-windows-wallpapers-pictures/1-2.%20Windows%203.1x%20and%20NT%203.x/SLASH.BMP"
+  "https://archive.org/download/microsoft-windows-wallpapers-pictures/1-2.%20Windows%203.1x%20and%20NT%203.x/SPOTS.BMP"
+  "https://archive.org/download/microsoft-windows-wallpapers-pictures/1-2.%20Windows%203.1x%20and%20NT%203.x/STEEL.BMP"
+  "https://archive.org/download/microsoft-windows-wallpapers-pictures/1-2.%20Windows%203.1x%20and%20NT%203.x/TARTAN.BMP"
+  "https://archive.org/download/microsoft-windows-wallpapers-pictures/1-2.%20Windows%203.1x%20and%20NT%203.x/arcade.bmp"
+  "https://archive.org/download/microsoft-windows-wallpapers-pictures/1-2.%20Windows%203.1x%20and%20NT%203.x/arches.bmp"
+  "https://archive.org/download/microsoft-windows-wallpapers-pictures/1-2.%20Windows%203.1x%20and%20NT%203.x/argyle.bmp"
+  "https://archive.org/download/microsoft-windows-wallpapers-pictures/1-2.%20Windows%203.1x%20and%20NT%203.x/ball.bmp"
+  "https://archive.org/download/microsoft-windows-wallpapers-pictures/1-2.%20Windows%203.1x%20and%20NT%203.x/cars.bmp"
+  "https://archive.org/download/microsoft-windows-wallpapers-pictures/1-2.%20Windows%203.1x%20and%20NT%203.x/castle.bmp"
+  "https://archive.org/download/microsoft-windows-wallpapers-pictures/1-2.%20Windows%203.1x%20and%20NT%203.x/chitz.bmp"
+  "https://archive.org/download/microsoft-windows-wallpapers-pictures/1-2.%20Windows%203.1x%20and%20NT%203.x/egypt.bmp"
+  "https://archive.org/download/microsoft-windows-wallpapers-pictures/1-2.%20Windows%203.1x%20and%20NT%203.x/honey.bmp"
+  "https://archive.org/download/microsoft-windows-wallpapers-pictures/1-2.%20Windows%203.1x%20and%20NT%203.x/lanma256%20%28NT%203.51%29.bmp"
+  "https://archive.org/download/microsoft-windows-wallpapers-pictures/1-2.%20Windows%203.1x%20and%20NT%203.x/lanmannt%20%28NT%203.5%29.bmp"
+  "https://archive.org/download/microsoft-windows-wallpapers-pictures/1-2.%20Windows%203.1x%20and%20NT%203.x/lanmannt%20%28NT%203.51%29.bmp"
+  "https://archive.org/download/microsoft-windows-wallpapers-pictures/1-2.%20Windows%203.1x%20and%20NT%203.x/leaves.bmp"
+  "https://archive.org/download/microsoft-windows-wallpapers-pictures/1-2.%20Windows%203.1x%20and%20NT%203.x/marble.bmp"
+  "https://archive.org/download/microsoft-windows-wallpapers-pictures/1-2.%20Windows%203.1x%20and%20NT%203.x/redbrick.bmp"
+  "https://archive.org/download/microsoft-windows-wallpapers-pictures/1-2.%20Windows%203.1x%20and%20NT%203.x/rivets.bmp"
+  "https://archive.org/download/microsoft-windows-wallpapers-pictures/1-2.%20Windows%203.1x%20and%20NT%203.x/squares.bmp"
+  "https://archive.org/download/microsoft-windows-wallpapers-pictures/1-2.%20Windows%203.1x%20and%20NT%203.x/tartan%20%28build%20061d%29.bmp"
+  "https://archive.org/download/microsoft-windows-wallpapers-pictures/1-2.%20Windows%203.1x%20and%20NT%203.x/thatch.bmp"
+  "https://archive.org/download/microsoft-windows-wallpapers-pictures/1-2.%20Windows%203.1x%20and%20NT%203.x/winlogo.bmp"
+  "https://archive.org/download/microsoft-windows-wallpapers-pictures/1-2.%20Windows%203.1x%20and%20NT%203.x/winnt%20%28NT%203.5%29.bmp"
+  "https://archive.org/download/microsoft-windows-wallpapers-pictures/1-2.%20Windows%203.1x%20and%20NT%203.x/winnt%20%28NT%203.51%29.bmp"
+  "https://archive.org/download/microsoft-windows-wallpapers-pictures/1-2.%20Windows%203.1x%20and%20NT%203.x/winnt256%20%28NT%203.51%29.bmp"
+  "https://archive.org/download/microsoft-windows-wallpapers-pictures/1-2.%20Windows%203.1x%20and%20NT%203.x/zigzag.bmp"
+  "https://archive.org/download/microsoft-windows-wallpapers-pictures/1-1.%20Windows%203.0/BOXES.BMP"
+  "https://archive.org/download/microsoft-windows-wallpapers-pictures/1-1.%20Windows%203.0/CHESS.BMP"
+  "https://archive.org/download/microsoft-windows-wallpapers-pictures/1-1.%20Windows%203.0/PAPER.BMP"
+  "https://archive.org/download/microsoft-windows-wallpapers-pictures/1-1.%20Windows%203.0/PARTY.BMP"
+  "https://archive.org/download/microsoft-windows-wallpapers-pictures/1-1.%20Windows%203.0/PYRAMID.BMP"
+  "https://archive.org/download/microsoft-windows-wallpapers-pictures/1-1.%20Windows%203.0/RIBBONS.BMP"
+  "https://archive.org/download/microsoft-windows-wallpapers-pictures/1-1.%20Windows%203.0/WEAVE.BMP"
+)
+
+TARGET_DIR="/usr/share/backgrounds/Chicago95/Wallpaper"
+
+printf "%s\n" "${URLS[@]}" | xargs -n 1 -P 10 -I{} wget -P "$TARGET_DIR" "{}"
