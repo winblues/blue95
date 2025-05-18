1d2a1b2d05f0e7c8128568945bdd4d5bfb72dbb0
Adam Fidel <adam@fidel.id>
2025-05-17 20:25:11 -0500

    move install to 60-*

--- files/scripts/60-installer-support.sh
+++ files/scripts/60-installer-support.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-set -oue pipefail
+set -uxeo pipefail
+
+sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/ublue-os-staging-fedora-*.repo
+dnf5 install -y taidan
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/ublue-os-staging-fedora-*.repo
 
 systemctl enable taidan-initial-setup-reconfiguration.service
