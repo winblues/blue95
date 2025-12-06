24d085b6bd3ad3d6579f50e27416d3c4ddf2fdad
Adam Fidel <ledif@users.noreply.github.com>
2025-12-06 08:25:47 -0600

    chore: add cleanup code to build recipe (#87)

--- files/scripts/90-finalize.sh
+++ files/scripts/90-finalize.sh
@@ -8,7 +8,19 @@ gtk-update-icon-cache --force --ignore-theme-index /usr/share/icons/Chicago95
 
 systemctl --global preset-all
 
-# Clean up
-rm -rf /tmp/* /tmp/.* 2>/dev/null || true
-rm -rf /var/tmp/* /var/tmp/.* 2>/dev/null || true
+# Clean up temporary files and build artifacts
+find /tmp -mindepth 1 -maxdepth 1 -exec rm -rf {} + 2>/dev/null || true
+find /var/tmp -mindepth 1 -maxdepth 1 -exec rm -rf {} + 2>/dev/null || true
+
 rm -rf /var/cache/dnf/* 2>/dev/null || true
+rm -rf /var/lib/dnf/* 2>/dev/null || true
+
+# No idea where these come from, but try to remove them here to fix broken installer
+rm -f /nvim.root 2>/dev/null || true
+rm -f /dnf 2>/dev/null || true
+rm -rf /selinux-policy 2>/dev/null || true
+
+cd /
+for file in .wget-hsts* .wget-hpkp* .wh.* .*_lck_*; do
+  rm -rf "$file" 2>/dev/null || true
+done
